2e8878acb79ed566670efa7345cf643257755316
Divyansh Garg <divyansh.garg@joshtechnologygroup.com>
2026-09-16 14:45:39 +0530

    [DG_A2_02]: Removed wrongly placed navigation file

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
             '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
             '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
             '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+            '@config': fileURLToPath(new URL('./src/config', import.meta.url)),
             '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
             '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
             '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
