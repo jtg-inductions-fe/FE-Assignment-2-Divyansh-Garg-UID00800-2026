84741dad70d61f1195df69e8ac939dc758b22f8a
Divyansh Garg <divyansh.garg@joshtechnologygroup.com>
2026-09-18 12:48:54 +0530

    [DG_A2_05]: Added the follow/unfollow functionality in profile page

--- src/components/Common/FollowBtn.tsx
+++ src/components/Common/FollowBtn.tsx
@@ -2,24 +2,45 @@ import { colors } from '@theme/colors';
 import { pxToRem } from '@theme/functions';
 import { Button, styled } from '@mui/material';
 
-export const FollowButton = styled(Button)(({ theme }) => ({
+interface FollowButtonProps {
+    isFollowed: boolean;
+    onClick?: VoidFunction;
+}
+
+const StyledFollowButton = styled(Button)(({ theme }) => ({
     paddingLeft: theme.variables.spacing.lg,
     paddingRight: theme.variables.spacing.lg,
     borderRadius: theme.variables.radius.pill,
     fontWeight: theme.variables.fontWeight.semiBold,
     transition: theme.variables.transitions.normal,
-
     flexShrink: 0,
     minWidth: pxToRem(120),
     minHeight: pxToRem(44),
-    color: colors.white,
-    backgroundColor: colors.primary[700],
+})) as typeof Button;
 
-    '&:hover': {
-        backgroundColor: colors.primary[800],
-    },
-
-    [theme.breakpoints.down('md')]: {
-        width: '100%',
-    },
-}));
+export const FollowButton = ({ isFollowed, onClick }: FollowButtonProps) => {
+    return (
+        <StyledFollowButton
+            onClick={onClick}
+            sx={
+                isFollowed
+                    ? {
+                          color: colors.white,
+                          backgroundColor: colors.secondary[900],
+                          '&:hover': {
+                              backgroundColor: colors.secondary[500],
+                          },
+                      }
+                    : {
+                          color: colors.white,
+                          backgroundColor: colors.primary[900],
+                          '&:hover': {
+                              backgroundColor: colors.primary[500],
+                          },
+                      }
+            }
+        >
+            {isFollowed ? 'Unfollow' : 'Follow'}
+        </StyledFollowButton>
+    );
+};
