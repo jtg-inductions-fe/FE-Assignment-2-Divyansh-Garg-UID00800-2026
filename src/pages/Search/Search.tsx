005d3d94cb85e0960b5a745245f0e2fa268a54b0
Divyansh Garg <divyansh.garg@joshtechnologygroup.com>
2026-09-19 09:41:24 +0530

    [DG_A2_04]: Updated Search Page with previous branch and used hook for API call

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,5 +1,4 @@
-import { Fragment, useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router';
+import { Fragment } from 'react';
 
 import { Close, Search as SearchIcon } from '@mui/icons-material';
 import { Autocomplete, CircularProgress, IconButton, TextField, Typography } from '@mui/material';
