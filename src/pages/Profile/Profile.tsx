4275cc5128c53da3f9f437324463fcfca03f54b1
Divyansh Garg <divyansh.garg@joshtechnologygroup.com>
2026-09-19 15:34:57 +0530

    [DG_A2_05]: Updated with previous branch and fixed reviews

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -3,23 +3,12 @@ import { colors } from '@theme/colors';
 import { pxToRem } from '@theme/functions';
 
 import { Page } from '@components/common/Page';
-import { Card } from '@components/common/Card';
+import { Card, CardMain, Label, Value } from '@components/common/Card';
 import { Content } from '@components/common/Content';
-import { Title } from '@components/common/Header';
 import {
-    AdditionalInfo,
-    Bio,
-    Count,
-    Eyebrow,
-    GridEle,
-    GridEleText,
-    ProfileAvatar,
     ProfileCard,
-    ProfileCardHeader,
-    ProfileCardHeaderBottom,
     ProfileCardHeaderTop,
     ProfileCardTopHeaderInner,
-    ProfileMain,
     ProfileMainTop,
     ProfileUsername,
     StatValue,
@@ -27,180 +16,21 @@ import {
 
 import { FollowButton } from '@components/common/FollowBtn';
 import { ErrorBox } from '@components/common/ErrorBox';
+import { CardHeader } from '@components/common/Card';
 
 import { ArrowOutward } from '@mui/icons-material';
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, Typography } from '@mui/material';
 
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-
-import { useAppDispatch, useAppSelector } from '@app/hooks';
-import { snakeToCamelCase } from '@utils/helperFunctions';
-import type { AuthUser } from '@features/auth/authTypes';
-
-import type { SocialUser } from '@features/social/socialTypes';
-import { addFollower, removeFollower } from '@features/social/socialSlice';
+import { StyledAvatar } from '@components/common/StyledAvatar';
+import { Grid, GridEle } from '@components/common/Grid';
+import { Count, CountBox } from '@components/common/Count';
+import { useGithubProfile } from '@components/profile/useGithubProfile';
+import { useGithubSocial } from '@components/follow/useGithubSocial';
 
 const Profile = () => {
-    const { username } = useParams<{ username: string }>();
+    const { isFollowed, searchUserInfo, isOwnProfile, error, isAuthenticated } = useGithubProfile();
 
-    const dispatch = useAppDispatch();
-
-    const [searchUserInfo, setSearchUserInfo] = useState<AuthUser | null>(null);
-
-    const [_, setLoading] = useState(false);
-    const [error, setError] = useState('');
-
-    const authUser = useAppSelector((state) => state.auth.user);
-    const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
-    const token = useAppSelector((state) => state.auth.token);
-    const isFetched = useAppSelector((state) => state.social.isFetched);
-    const following = useAppSelector((state) => state.social.following);
-
-    let isFollowed = false;
-
-    const isOwnProfile =
-        isAuthenticated &&
-        !!authUser &&
-        !!username &&
-        authUser.login.toLowerCase() === username.toLowerCase();
-
-    useEffect(() => {
-        const handleUserSearch = async (value: string) => {
-            const trimmedUsername = value.trim();
-
-            if (!trimmedUsername) {
-                setSearchUserInfo(null);
-                return;
-            }
-
-            setLoading(true);
-            setError('');
-
-            try {
-                const response = await fetch(`https://api.github.com/users/${trimmedUsername}`, {
-                    headers: {
-                        Accept: 'application/vnd.github+json',
-                    },
-                });
-
-                if (!response.ok) {
-                    if (response.status === 404) {
-                        throw new Error('User Not Found');
-                    }
-
-                    throw new Error('Unable to get the GitHub user profile.');
-                }
-
-                const data = await response.json();
-
-                const formattedData = snakeToCamelCase<typeof data, AuthUser>(data);
-
-                setSearchUserInfo(formattedData);
-            } catch (err) {
-                setSearchUserInfo(null);
-
-                setError(
-                    err instanceof Error
-                        ? err.message
-                        : 'Something went wrong while connecting to GitHub.',
-                );
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        if (username) {
-            handleUserSearch(username);
-        }
-    }, [username]);
-
-    const handleFollowUnfollow = async (isFollowed: boolean, user: SocialUser | null) => {
-        if (!user) {
-            setError('The Following User is not available');
-            return;
-        }
-
-        const trimmedToken = token?.trim();
-
-        if (!trimmedToken) {
-            setError('Please Login First to follow.');
-            return;
-        }
-
-        setError('');
-        setLoading(true);
-
-        if (isFollowed) {
-            try {
-                const response = await fetch(
-                    `https://api.github.com/user/following/${user.login}`,
-                    {
-                        method: 'DELETE',
-                        headers: {
-                            Authorization: `Bearer ${trimmedToken}`,
-                            Accept: 'application/vnd.github+json',
-                            'Content-Length': '0',
-                        },
-                    },
-                );
-
-                if (!response.ok) {
-                    if (response.status === 401) {
-                        throw new Error('You are not Authorized to follow the user');
-                    }
-
-                    throw new Error('Unable to Follow the user.');
-                }
-
-                dispatch(removeFollower(user));
-                isFollowed = false;
-            } catch (error) {
-                setError(
-                    error instanceof Error
-                        ? error.message
-                        : 'Something went wrong while connecting to GitHub.',
-                );
-            } finally {
-                setLoading(false);
-            }
-        } else {
-            try {
-                const response = await fetch(
-                    `https://api.github.com/user/following/${user.login}`,
-                    {
-                        method: 'PUT',
-                        headers: {
-                            Authorization: `Bearer ${trimmedToken}`,
-                            Accept: 'application/vnd.github+json',
-                            'Content-Length': '0',
-                        },
-                    },
-                );
-
-                if (!response.ok) {
-                    if (response.status === 401) {
-                        throw new Error('You are not Authorized to Unfollow the user');
-                    }
-
-                    throw new Error('Unable to Unfollow the user.');
-                }
-
-                dispatch(addFollower(user));
-                isFollowed = true;
-            } catch (error) {
-                setError(
-                    error instanceof Error
-                        ? error.message
-                        : 'Something went wrong while connecting to GitHub.',
-                );
-            } finally {
-                setLoading(false);
-            }
-        }
-    };
-
-    isFollowed = Boolean(searchUserInfo && String(searchUserInfo.id) in following);
+    const { isFetched, handleFollowUnfollow } = useGithubSocial();
 
     return (
         <Page>
@@ -228,21 +58,25 @@ const Profile = () => {
                 }}
             >
                 <Card>
-                    <Title variant="h3">{isOwnProfile ? 'My Profile' : 'Profile'}</Title>
+                    <Typography variant="h3">{isOwnProfile ? 'My Profile' : 'Profile'}</Typography>
 
                     {error ? (
                         <ErrorBox color="error">{error}</ErrorBox>
                     ) : (
                         <ProfileCard>
-                            <ProfileCardHeader>
+                            <CardHeader>
                                 <ProfileCardHeaderTop>
                                     {searchUserInfo?.avatarUrl ? (
-                                        <ProfileAvatar
+                                        <StyledAvatar
+                                            sx={{
+                                                width: '90px',
+                                                height: '90px',
+                                            }}
                                             src={searchUserInfo.avatarUrl}
                                             alt={searchUserInfo.login}
                                         />
                                     ) : (
-                                        <ProfileAvatar />
+                                        <StyledAvatar />
                                     )}
 
                                     <ProfileCardTopHeaderInner>
@@ -262,36 +96,36 @@ const Profile = () => {
                                                 </IconButton>
                                             )}
                                         </Box>
-                                        <ProfileUsername variant="body1">
+                                        <ProfileUsername variant="h6">
                                             {searchUserInfo?.name ? searchUserInfo.name : 'NA'}
                                         </ProfileUsername>
                                     </ProfileCardTopHeaderInner>
                                 </ProfileCardHeaderTop>
 
-                                <ProfileCardHeaderBottom>
+                                <CountBox>
                                     <Count>
-                                        <StatValue>
+                                        <Typography>
                                             Followers: {searchUserInfo?.followers ?? 'NA'}
-                                        </StatValue>
+                                        </Typography>
                                     </Count>
 
                                     <Count>
-                                        <StatValue>
+                                        <Typography>
                                             Following: {searchUserInfo?.following ?? 'NA'}
-                                        </StatValue>
+                                        </Typography>
                                     </Count>
-                                </ProfileCardHeaderBottom>
-                            </ProfileCardHeader>
+                                </CountBox>
+                            </CardHeader>
 
-                            <ProfileMain>
+                            <CardMain>
                                 <ProfileMainTop>
-                                    <Eyebrow variant="h6">
+                                    <Label variant="h6">
                                         {searchUserInfo?.email ?? 'Email is not available.'}
-                                    </Eyebrow>
+                                    </Label>
 
-                                    <Bio variant="h6">
+                                    <Typography variant="h6">
                                         {searchUserInfo?.bio ?? 'Bio is not available.'}
-                                    </Bio>
+                                    </Typography>
                                 </ProfileMainTop>
 
                                 {isAuthenticated && !isOwnProfile && isFetched && (
@@ -302,78 +136,68 @@ const Profile = () => {
                                         }
                                     />
                                 )}
-                            </ProfileMain>
+                            </CardMain>
 
                             {isOwnProfile && searchUserInfo && (
-                                <Box>
-                                    <AdditionalInfo>
-                                        <GridEle>
-                                            <Eyebrow variant="h6">Name:</Eyebrow>
-                                            <GridEleText>
-                                                {searchUserInfo.name ?? 'Not available'}
-                                            </GridEleText>
-                                        </GridEle>
+                                <Grid>
+                                    <GridEle>
+                                        <Label variant="h6">Name:</Label>
+                                        <Value>{searchUserInfo.name ?? 'Not available'}</Value>
+                                    </GridEle>
 
-                                        <GridEle>
-                                            <Eyebrow variant="h6">Company:</Eyebrow>
-                                            <GridEleText>
-                                                {searchUserInfo.company ?? 'Not available'}
-                                            </GridEleText>
-                                        </GridEle>
+                                    <GridEle>
+                                        <Label variant="h6">Company:</Label>
+                                        <Value>{searchUserInfo.company ?? 'Not available'}</Value>
+                                    </GridEle>
 
-                                        <GridEle>
-                                            <Eyebrow variant="h6">Location:</Eyebrow>
-                                            <GridEleText>
-                                                {searchUserInfo.location ?? 'Not available'}
-                                            </GridEleText>
-                                        </GridEle>
+                                    <GridEle>
+                                        <Label variant="h6">Location:</Label>
+                                        <Value>{searchUserInfo.location ?? 'Not available'}</Value>
+                                    </GridEle>
 
-                                        <GridEle>
-                                            <Eyebrow variant="h6">Blog:</Eyebrow>
-                                            <GridEleText>
-                                                {searchUserInfo.blog || 'Not available'}
-                                            </GridEleText>
-                                        </GridEle>
+                                    <GridEle>
+                                        <Label variant="h6">Blog:</Label>
+                                        <Value>{searchUserInfo.blog || 'Not available'}</Value>
+                                    </GridEle>
 
-                                        <GridEle>
-                                            <Eyebrow variant="h6">GitHub ID:</Eyebrow>
-                                            <GridEleText>{searchUserInfo.id}</GridEleText>
-                                        </GridEle>
+                                    <GridEle>
+                                        <Label variant="h6">GitHub ID:</Label>
+                                        <Value>{searchUserInfo.id}</Value>
+                                    </GridEle>
 
-                                        <GridEle>
-                                            <Eyebrow variant="h6">Account Type:</Eyebrow>
-                                            <GridEleText>{searchUserInfo.type}</GridEleText>
-                                        </GridEle>
+                                    <GridEle>
+                                        <Label variant="h6">Account Type:</Label>
+                                        <Value>{searchUserInfo.type}</Value>
+                                    </GridEle>
 
-                                        <GridEle>
-                                            <Eyebrow variant="h6">Public Repositories:</Eyebrow>
-                                            <GridEleText>{searchUserInfo.publicRepos}</GridEleText>
-                                        </GridEle>
+                                    <GridEle>
+                                        <Label variant="h6">Public Repositories:</Label>
+                                        <Value>{searchUserInfo.publicRepos}</Value>
+                                    </GridEle>
 
-                                        <GridEle>
-                                            <Eyebrow variant="h6">Public Gists:</Eyebrow>
-                                            <GridEleText>{searchUserInfo.publicGists}</GridEleText>
-                                        </GridEle>
+                                    <GridEle>
+                                        <Label variant="h6">Public Gists:</Label>
+                                        <Value>{searchUserInfo.publicGists}</Value>
+                                    </GridEle>
 
-                                        <GridEle>
-                                            <Eyebrow variant="h6">Account Created: </Eyebrow>
-                                            <GridEleText>
-                                                {searchUserInfo.createdAt
-                                                    ? searchUserInfo.createdAt
-                                                    : 'Not available'}
-                                            </GridEleText>
-                                        </GridEle>
+                                    <GridEle>
+                                        <Label variant="h6">Account Created: </Label>
+                                        <Value>
+                                            {searchUserInfo.createdAt
+                                                ? new Date(searchUserInfo.createdAt).toDateString()
+                                                : 'Not available'}
+                                        </Value>
+                                    </GridEle>
 
-                                        <GridEle>
-                                            <Eyebrow variant="h6">Last Updated: </Eyebrow>
-                                            <GridEleText>
-                                                {searchUserInfo.updatedAt
-                                                    ? searchUserInfo.updatedAt
-                                                    : 'Not available'}
-                                            </GridEleText>
-                                        </GridEle>
-                                    </AdditionalInfo>
-                                </Box>
+                                    <GridEle>
+                                        <Label variant="h6">Last Updated: </Label>
+                                        <Value>
+                                            {searchUserInfo.updatedAt
+                                                ? new Date(searchUserInfo.updatedAt).toDateString()
+                                                : 'Not available'}
+                                        </Value>
+                                    </GridEle>
+                                </Grid>
                             )}
                         </ProfileCard>
                     )}
