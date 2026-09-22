75b5af0f5546058348406531158b75c9312d2032
Divyansh Garg <divyansh.garg@joshtechnologygroup.com>
2026-09-22 09:03:51 +0530

    [DG_A2_05]: Did the latest review fixes

--- src/pages/Profile/useGithubProfile.ts
+++ src/pages/Profile/useGithubProfile.ts
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react';
 
-import type { AuthUser } from '@redux/auth/authTypes';
+import type { AuthUser } from '@redux/auth';
 
 import { fetchGitHubProfile } from '@utils/services/githubProfile';
 import { useAppSelector } from './storeHooks';
